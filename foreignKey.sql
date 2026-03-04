bf1aa3a3f7a2d8d09b5091d0bb726354d8197da9
nizam <nizam.shejan11@gmail.com>
2026-03-04 14:35:49 +0600

    practice foreign key

---
+++ foreignKey.sql
@@ -0,0 +1,18 @@
+
+-- Module 9: 3
+CREATE TABLE "user"(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(25) NOT NULL
+)
+
+CREATE TABLE post(
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    user_id INTEGER REFERENCES "user"(id)
+)
+
+
+-- Module 9: 4, 5 
+-- here discuss some issue llike if delete any user then delete the post which post has foreign key from user primary id that means deletation condition/rules here
+
+
